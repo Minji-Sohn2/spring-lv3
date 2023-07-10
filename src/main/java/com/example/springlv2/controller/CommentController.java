4b053fa75d047b74103eb10b702e85f2171cc8b9
Minji-Sohn2 <cyzh1311@gmail.com>
2023-07-10 15:36:39 +0900

    댓글 삭제 구현, admin 수정/삭제 코드 수정, comment 경로 수정

--- src/main/java/com/example/springlv2/controller/CommentController.java
+++ src/main/java/com/example/springlv2/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.example.springlv2.controller;
 
+import com.example.springlv2.dto.ApiResponseDto;
 import com.example.springlv2.dto.CommentRequestDto;
 import com.example.springlv2.dto.CommentResponseDto;
 import com.example.springlv2.security.UserDetailsImpl;
@@ -9,19 +10,33 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/memos")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/comments/{memoId}")
+    @PostMapping("/{memoId}/comments")
     public CommentResponseDto createComment(@PathVariable Long memoId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.createComment(memoId,requestDto, userDetails);
     }
 
-    @PutMapping("/comments/{memoId}/{commentId}")
-    public CommentResponseDto updateComment(@PathVariable Long memoId, @PathVariable Long commentId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    @PutMapping("/{memoId}/comments/{commentId}")
+    public CommentResponseDto updateComment(
+            @PathVariable Long memoId,
+            @PathVariable Long commentId,
+            @RequestBody CommentRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         return commentService.update(memoId, commentId, requestDto, userDetails);
+    }
+
+    @DeleteMapping("/{memoId}/comments/{commentId}")
+    public ApiResponseDto deleteComment(
+            @PathVariable Long memoId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        return commentService.deleteComment(memoId, commentId, userDetails);
     }
 }
--- src/main/java/com/example/springlv2/service/CommentService.java
+++ src/main/java/com/example/springlv2/service/CommentService.java
@@ -1,13 +1,16 @@
 package com.example.springlv2.service;
 
+import com.example.springlv2.dto.ApiResponseDto;
 import com.example.springlv2.dto.CommentRequestDto;
 import com.example.springlv2.dto.CommentResponseDto;
 import com.example.springlv2.entity.Comment;
 import com.example.springlv2.entity.Memo;
+import com.example.springlv2.entity.UserRoleEnum;
 import com.example.springlv2.repository.CommentRepository;
 import com.example.springlv2.repository.MemoRepository;
 import com.example.springlv2.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,24 +32,42 @@ public class CommentService {
         return new CommentResponseDto(commentRepository.save(comment));
     }
 
-    public List<CommentResponseDto> getCommentList(Long memoId) {
-        return commentRepository.findAllByMemo_IdOrderByCreatedAtDesc(memoId).stream().map(CommentResponseDto::new).toList();
-    }
-
     @Transactional
     public CommentResponseDto update(Long memoId, Long commentId, CommentRequestDto requestDto, UserDetailsImpl userDetails) {
-        Memo memo = findMemo(memoId);
+        findMemo(memoId);
         Comment comment = findComment(commentId);
-        comment.checkUsername(userDetails.getUsername());
 
-        comment.setComment(requestDto.getComment());
+        if(userDetails.getUser().getRole().equals(UserRoleEnum.ADMIN)) {
+            comment.setComment(requestDto.getComment());
+        } else {
+            comment.checkUsername(userDetails.getUsername());
+
+            comment.setComment(requestDto.getComment());
+        }
+
         return new CommentResponseDto(comment);
+    }
+
+    public ApiResponseDto deleteComment(Long memoId, Long commentId, UserDetailsImpl userDetails) {
+        findMemo(memoId);
+        Comment comment = findComment(commentId);
+
+        if(userDetails.getUser().getRole().equals(UserRoleEnum.ADMIN)) {
+            commentRepository.delete(comment);
+        } else {
+            comment.checkUsername(userDetails.getUsername());
+
+            commentRepository.delete(comment);
+        }
+
+        return new ApiResponseDto("댓글 삭제 완료", HttpStatus.OK.value());
     }
 
     private Memo findMemo(Long memoId) {
         return memoRepositoy.findById(memoId).orElseThrow(() ->
                 new IllegalArgumentException("선택한 메모는 존재하지 않습니다."));
     }
+
     private Comment findComment(Long commentId) {
         return commentRepository.findById(commentId).orElseThrow(() ->
                 new IllegalArgumentException("선택한 댓글은 존재하지 않습니다."));
